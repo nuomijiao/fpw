cef66cfc1d4ae866599e4b819907d16f758e45f3
DingBaiJiao <dbj_1991@163.com>
2018-05-03 17:22:57 +0800

    123

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -20,6 +20,9 @@ class Index extends Controller
                 header("Location:$jumpUrl");
             } else {
                 $accessToken = $oAuth->oAuthAccessToken($param['code']);
+                echo "<pre>";
+                print_r($accessToken);
+                echo "</pre>"die;
                 $userInfo = $oAuth->oAuthGetUserInfo($accessToken->access_token, $accessToken->openid);
                 return json_decode($userInfo, true);
             }
