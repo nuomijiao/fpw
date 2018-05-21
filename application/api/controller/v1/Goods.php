8dbcc1ebf5c1d859e2fce6c70a4bf07735b437b7
DingBaiJiao <dbj_1991@163.com>
2018-05-21 10:11:56 +0800

    2323

--- application/api/controller/v1/Goods.php
+++ application/api/controller/v1/Goods.php
@@ -162,6 +162,7 @@ class Goods extends BaseController
     public function autoDelTmpPic()
     {
         $delPic = TmpPicModel::getDelPic();
+        return json($delPic);
         if (!$delPic->isEmpty()) {
             $picArray = $delPic->toArray();
             $picImgUrl = [];
