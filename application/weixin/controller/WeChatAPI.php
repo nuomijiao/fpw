8e2f213bce59cb82823a2fab294d481ce52f5533
DingBaiJiao <dbj_1991@163.com>
2018-05-25 16:16:49 +0800

    ï32323

--- application/weixin/controller/WeChatAPI.php
+++ application/weixin/controller/WeChatAPI.php
@@ -21,7 +21,7 @@ class WeChatAPI extends BaseController
         $this->appID = config('weixin.appID');
         $this->appSecret = config('weixin.appSecret');
 
-        $res = file_get_contents('wx_access_token/access_token.json');
+        $res = file_get_contents(config('weixin.accessTokenUrl'));
         $result = json_decode($res, true);
         $this->accessToken = $result['access_token'];
         $expires_time = $result['expires_time'];
